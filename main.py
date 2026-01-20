75b171b04c4448c272b6138bf82e36d8f21dc489
myuu <46139605+myuubunny@users.noreply.github.com>
2026-01-20 16:39:29 -0300

    original_message to original_response

--- main.py
+++ main.py
@@ -32,33 +32,33 @@ async def sync(interaction):
 @bot.tree.command(name="eq", description="English Qaballa cipher")
 async def eq(interaction, *, arg: str):
     await interaction.response.send_message("English Qaballa: " + str(pygematria.conv.string_values(arg, 'eq')) + " = " + str(sum(pygematria.conv.string_values(arg, 'eq'))))
-    msg = await interaction.original_message()
+    msg = await interaction.original_response()
 
 @bot.tree.command(name="a", description="Agrippa cipher")
 async def a(interaction, *, arg: str):
     await interaction.response.send_message("Agrippa: " + str(pygematria.conv.string_values(arg, 'agrippa')) + " = " + str(sum(pygematria.conv.string_values(arg, 'agrippa'))))
-    msg = await interaction.original_message()
+    msg = await interaction.original_response()
 
 @bot.tree.command(name="h", description="Hebrew cipher")
 async def h(interaction, *, arg: str):
     await interaction.response.send_message("Hebrew: " + str(pygematria.conv.string_values(arg, 'hebrew')) + " = " + str(sum(pygematria.conv.string_values(arg, 'hebrew'))))
-    msg = await interaction.original_message()
+    msg = await interaction.original_response()
 
 @bot.tree.command(name="g", description="Greek isopsephy cipher")
 async def g(interaction, *, arg: str):
     await interaction.response.send_message("Greek isopsephy:" + str(pygematria.conv.string_values(arg, 'greek')) + " = " + str(sum(pygematria.conv.string_values(arg, 'greek'))))
-    msg = await interaction.original_message()
+    msg = await interaction.original_response()
 
 @bot.tree.command(name="r", description="Rudolff cipher")
 async def r(interaction, *, arg: str):
     await interaction.response.send_message("Rudolff: " + str(pygematria.conv.string_values(arg, 'rudolff')) + " = " + str(sum(pygematria.conv.string_values(arg, 'rudolff'))))
-    msg = await interaction.original_message()
+    msg = await interaction.original_response()
 
 @bot.tree.command(name="fortune", description="s4s fortune")
 async def fortune(interaction):
     fortune = ['Your Fortune: Reply hazy, try again', 'Your Fortune: Excellent Luck', 'Your Fortune: Good Luck', 'Your Fortune: Average Luck', 'Your Fortune: Bad Luck', 'Your Fortune: Good news will come to you by mail', 'Your Fortune: （  `_ゝ`）ﾌｰﾝ ', 'Your Fortune: ｷﾀ━━━━━━(ﾟ∀ﾟ)━━━━━━ !!!!', 'Your Fortune: You will meet a dark handsome stranger', 'Your Fortune: Better not tell you now', 'Your Fortune: Outlook good', 'Your Fortune: Very Bad Luck', 'Your Fortune: Godly Luck']
     await interaction.response.send_message(random.choice(fortune))
-    msg = await interaction.original_message()
+    msg = await interaction.original_response()
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
 
